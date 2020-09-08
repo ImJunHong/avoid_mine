fdadd9738fa05ce60730c97ce7b05d8b6c2d55f8
ImJunHong <tesseract4@naver.com>
2020-09-08 17:45:26 +0900

    add:class

--- avoid_mine.py
+++ avoid_mine.py
@@ -2,95 +2,56 @@ import pygame as pg
 import random
 import sys
 
-background_color = (50, 50, 50)
-black = (0, 0, 0)
-white = (255, 255, 255)
-dark_white = (235, 235, 235)
-cyan = (100, 180, 255)
-green = (51, 204, 0)
-blue = (0, 0, 255)
-red = (255, 0, 0)
-yellow = (255, 255, 0)
+background_color = ( 50,  50,  50)
+black            = (  0,   0,   0)
+white            = (255, 255, 255)
+dark_white       = (235, 235, 235)
+cyan             = (100, 180, 255)
+green            = ( 51, 204,   0)
+blue             = (  0,   0, 255)
+red              = (255,   0,   0)
+yellow           = (255, 255,   0)
 
 margin = 100
 cell_size = 40
 cell_num = 15
 screen_size = [margin*2 + cell_size*cell_num, margin*2 + cell_size*cell_num]
-coordinate = [[0 for y in range(cell_num)] for x in range(cell_num)]
-mine_coordinate = [[0 for y in range(cell_num)] for x in range(cell_num)]
-current_coordinate = [0, 0]
-secured_coordinate = [(0, 0)]
 initial_cells = [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
-show_list = []
+initial_mine_cells = [(0, 1), (1, 0), (1, 1)]
+
+fps = 60
+clock = pg.time.Clock()
 
 def main():
     pg.init()
     screen = pg.display.set_mode(screen_size)
-    font = pg.font.SysFont("Times New Roman", margin*3//10)
     pg.display.set_caption("지뢰피하기")
-    fps = 60
 
-    screen.fill(background_color)
-    draw_board(screen)
-    rects = draw_menu(font, screen)
-    pg.display.flip()
+    game = Game(screen)
+    button = Button(screen, game)
 
     while True:
-        for event in pg.event.get():
-            if event.type == pg.MOUSEBUTTONDOWN:
-                select_level(event.pos, rects, screen, font, fps)
-            elif event.type == pg.QUIT:
-                exit()
+        button.check_click()
 
-def select_level(pos, rects, screen, font, fps):
-    if rects[0].collidepoint(pos):
-        level = "easy"
-        coordinate = [[0 for y in range(cell_num)] for x in range(cell_num)]
-        mine_coordinate = [[0 for y in range(cell_num)] for x in range(cell_num)]
-        current_coordinate = [0, 0]
-        secured_coordinate = [(0, 0)]
-        show_list = []
-        start_game(screen, font, fps, level)
+def play_game(screen, game, level):
+    screen.fill(background_color)
+    game.init_game(level)
 
-def start_game(screen, font, fps, level):
-    set_path() # 시작점에서 도착점으로 가는 최단경로 설정
-    set_mines(level) # 지뢰 생성 및 지뢰 알림 숫자 설정
-    
-    clock = pg.time.Clock()
-    fail = False
-
-    while not fail:
-        clock.tick(fps)
-        screen.fill(background_color)
-        draw_secured_cells(screen)
-        draw_board(screen)
-        show_numbers(font, screen)
-        draw_me(screen)
-
+    while True:
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
                 pressed = pg.key.get_pressed()
                 keynames = [pg.key.name(key) for key, value in enumerate(pressed) if value]
-
-                for key in keynames:
-                    if key == "up" and current_coordinate[1] > 0:
-                        current_coordinate[1] -= 1
-                        fail = secure_cell(font, screen)
-                    elif key == "down" and current_coordinate[1] < cell_num-1:
-                        current_coordinate[1] += 1
-                        fail = secure_cell(font, screen)
-                    elif key == "left" and current_coordinate[0] > 0:
-                        current_coordinate[0] -= 1
-                        fail = secure_cell(font, screen)
-                    elif key == "right" and current_coordinate[0] < cell_num-1:
-                        current_coordinate[0] += 1
-                        fail = secure_cell(font, screen)
+                game.check_keypress(keynames)
 
             elif event.type == pg.QUIT:
-                exit()
+                game.exit()
+
+        if game.is_fail:
+            return
         
         pg.display.flip()
-    
+        clock.tick(fps)
 
 def print_text(font, screen, msg, color, pos):
     textSurface = font.render(msg, True, color, None)
@@ -99,131 +60,199 @@ def print_text(font, screen, msg, color, pos):
     screen.blit(textSurface, textRect)
     return textRect
 
-def set_path():
-    x, y = 0, 0
-    while x+y < (cell_num-1)*2:
-        if random.random() > 0.5 and x < cell_num-1:
-            x += 1
-            mine_coordinate[y][x] = "P"
-        elif y < cell_num-1:
-            y += 1
-            mine_coordinate[y][x] = "P"
+class Game(object):
+    def __init__(self, screen):
+        self.screen = screen
+        self.font = pg.font.SysFont("Times New Roman", margin*3//10)
 
-def set_mines(level):
-    if level == "easy":
-        mine_num = (cell_num**2)//3
-
-    minefield = list(range(1, cell_num**2))
-    for mine in range(1, cell_num**2):
-        if mine_coordinate[mine//cell_num][mine%cell_num] == "P":
-            minefield.remove(mine)
-    mine_list = random.sample(minefield, mine_num)
-    for mine in mine_list:
-        mine_X = mine%cell_num
-        mine_Y = mine//cell_num
-        mine_coordinate[mine_Y][mine_X] = "M"
+    def init_game(self, level):
+        self.coordinate = [[0 for y in range(cell_num)] for x in range(cell_num)]
+        self.mine_coordinate = [[0 for y in range(cell_num)] for x in range(cell_num)]
+        self.current_coordinate = [0, 0]
+        self.secured_coordinate = [(0, 0)]
+        self.show_list = [x for x in initial_cells]
+        self.showed_list = []
+        self.is_fail = False
+        self.set_path()
+        self.set_mines(level)
+        self.draw_secured_cells()
+        self.draw_board()
+        self.show_numbers()
+        self.draw_me()
         
-        coordinate[mine_Y][mine_X] += 1 # 지뢰가 있는 칸
-        if mine_Y > 0: # 상
-            coordinate[mine_Y - 1][mine_X] += 1
-        if mine_Y < cell_num-1: # 하
-            coordinate[mine_Y + 1][mine_X] += 1
-        if mine_X > 0: # 좌
-            coordinate[mine_Y][mine_X - 1] += 1
-        if mine_X < cell_num-1: # 우
-            coordinate[mine_Y][mine_X + 1] += 1
-        if mine_Y > 0 and mine_X > 0: # 좌상
-            coordinate[mine_Y - 1][mine_X - 1] += 1
-        if mine_Y < cell_num-1 and mine_X > 0: # 좌하
-            coordinate[mine_Y + 1][mine_X - 1] += 1
-        if mine_Y > 0 and mine_X < cell_num-1: # 우상
-            coordinate[mine_Y - 1][mine_X + 1] += 1
-        if mine_Y < cell_num-1 and mine_X < cell_num-1: # 우하
-            coordinate[mine_Y + 1][mine_X + 1] += 1
 
-    for i in mine_coordinate: print(i)
+    def draw_board(self):
+        for i in range(cell_num+1):
+            pg.draw.aaline(self.screen, white, [margin, margin + cell_size*i], [margin + cell_size*cell_num, margin + cell_size*i])
+            pg.draw.aaline(self.screen, white, [margin + cell_size*i, margin], [margin + cell_size*i, margin + cell_size*cell_num])
 
-def draw_secured_cells(screen):
-    for x, y in secured_coordinate:
-        pg.draw.rect(screen, dark_white, [margin + cell_size*x, margin + cell_size*y, cell_size, cell_size])
-    pg.draw.rect(screen, yellow, [margin + cell_size*(cell_num-1), margin + cell_size*(cell_num-1), cell_size, cell_size])
+    def check_keypress(self, keynames):
+        for key in keynames:
+            if key == "up" and self.current_coordinate[1] > 0:
+                self.current_coordinate[1] -= 1
+                self.is_fail = self.secure_cell()
+            elif key == "down" and self.current_coordinate[1] < cell_num-1:
+                self.current_coordinate[1] += 1
+                self.is_fail = self.secure_cell()
+            elif key == "left" and self.current_coordinate[0] > 0:
+                self.current_coordinate[0] -= 1
+                self.is_fail = self.secure_cell()
+            elif key == "right" and self.current_coordinate[0] < cell_num-1:
+                self.current_coordinate[0] += 1
+                self.is_fail = self.secure_cell()
 
-def draw_board(screen):
-    for i in range(cell_num+1):
-        pg.draw.aaline(screen, white, [margin, margin + cell_size*i], [margin + cell_size*cell_num, margin + cell_size*i])
-        pg.draw.aaline(screen, white, [margin + cell_size*i, margin], [margin + cell_size*i, margin + cell_size*cell_num])
+    def draw_secured_cells(self):
+        for x, y in self.secured_coordinate:
+            pg.draw.rect(self.screen, dark_white, [margin + cell_size*x, margin + cell_size*y, cell_size, cell_size])
+        pg.draw.rect(self.screen, yellow, [margin + cell_size*(cell_num-1), margin + cell_size*(cell_num-1), cell_size, cell_size])
 
-def show_numbers(font, screen):
-    for x, y in secured_coordinate:
-        if y > 0 and (x, y-1) not in show_list: # 상
-            show_list.append((x, y-1))
-        if y < cell_num-1 and (x, y+1) not in show_list: # 하
-            show_list.append((x, y+1))
-        if x > 0 and (x-1, y) not in show_list: # 좌
-            show_list.append((x-1, y))
-        if x < cell_num-1 and (x+1, y) not in show_list: # 우
-            show_list.append((x+1, y))
-        if x > 0 and y > 0 and (x-1, y-1) not in show_list: # 좌상
-            show_list.append((x-1, y-1))
-        if x > 0 and y < cell_num-1 and (x-1, y+1) not in show_list: # 좌하
-            show_list.append((x-1, y+1))
-        if x < cell_num-1 and y > 0 and (x+1, y-1) not in show_list: # 우상
-            show_list.append((x+1, y-1))
-        if x < cell_num-1 and y < cell_num-1 and (x+1, y+1) not in show_list: # 우하
-            show_list.append((x+1, y+1))
+    def show_numbers(self):
+        for x, y in self.secured_coordinate:
+            if y > 0 and (x, y-1) not in self.show_list: # 상
+                self.show_list.append((x, y-1))
+            if y < cell_num-1 and (x, y+1) not in self.show_list: # 하
+                self.show_list.append((x, y+1))
+            if x > 0 and (x-1, y) not in self.show_list: # 좌
+                self.show_list.append((x-1, y))
+            if x < cell_num-1 and (x+1, y) not in self.show_list: # 우
+                self.show_list.append((x+1, y))
+            if x > 0 and y > 0 and (x-1, y-1) not in self.show_list: # 좌상
+                self.show_list.append((x-1, y-1))
+            if x > 0 and y < cell_num-1 and (x-1, y+1) not in self.show_list: # 좌하
+                self.show_list.append((x-1, y+1))
+            if x < cell_num-1 and y > 0 and (x+1, y-1) not in self.show_list: # 우상
+                self.show_list.append((x+1, y-1))
+            if x < cell_num-1 and y < cell_num-1 and (x+1, y+1) not in self.show_list: # 우하
+                self.show_list.append((x+1, y+1))
 
-    for x, y in show_list:
-        value = coordinate[y][x]
-        text = str(value)
-        if value != "M" and value != cell_num**2-1:
-            if value < 3:
-                print_text(font, screen, text, blue,\
-                            (margin + cell_size//2 + cell_size*x, margin + cell_size//2 + cell_size*y))
-            elif value < 5:
-                print_text(font, screen, text, green,\
-                            (margin + cell_size//2 + cell_size*x, margin + cell_size//2 + cell_size*y))
-            else:
-                print_text(font, screen, text, red,\
-                            (margin + cell_size//2 + cell_size*x, margin + cell_size//2 + cell_size*y))
+        for x, y in self.show_list:
+            value = self.coordinate[y][x]
+            text = str(value)
+            if value != "M" and value != cell_num**2-1:
+                if (x, y) not in self.showed_list or (x, y) in self.secured_coordinate:
+                    self.showed_list.append((x, y))
+                    if value < 3:
+                        print_text(self.font, self.screen, text, blue,\
+                                    (margin + cell_size//2 + cell_size*x, margin + cell_size//2 + cell_size*y))
+                    elif value < 5:
+                        print_text(self.font, self.screen, text, green,\
+                                    (margin + cell_size//2 + cell_size*x, margin + cell_size//2 + cell_size*y))
+                    else:
+                        print_text(self.font, self.screen, text, red,\
+                                    (margin + cell_size//2 + cell_size*x, margin + cell_size//2 + cell_size*y))
 
-        bomb_img = pg.image.load("bomb.png")
-        bomb = pg.transform.scale(bomb_img, (cell_size, cell_size))
-    for x, y in initial_cells:
-        if mine_coordinate[y][x] == "M":
-            screen.blit(bomb, (margin + cell_size*x, margin + cell_size*y))
+            bomb_img = pg.image.load("bomb.png")
+            bomb = pg.transform.scale(bomb_img, (cell_size, cell_size))
+        for x, y in initial_mine_cells:
+            if self.mine_coordinate[y][x] == "M":
+                self.screen.blit(bomb, (margin + cell_size*x, margin + cell_size*y))
 
-def draw_me(screen):
-    pg.draw.rect(screen, cyan, [margin + cell_size//8 + cell_size*current_coordinate[0],\
-                                margin + cell_size//8 + cell_size*current_coordinate[1],\
-                                (cell_size*3)//4, (cell_size*3)//4])
+    def draw_me(self):
+        pg.draw.rect(self.screen, cyan, [margin + cell_size//8 + cell_size*self.current_coordinate[0],\
+                                    margin + cell_size//8 + cell_size*self.current_coordinate[1],\
+                                    (cell_size*3)//4, (cell_size*3)//4])
 
-def draw_menu(font, screen):
-    easy_rect = print_text(font, screen, "Easy", green, (margin + cell_size , (margin*3)//2 + cell_size*cell_num))
-    return [easy_rect]
+    def secure_cell(self):
+        if self.mine_coordinate[self.current_coordinate[1]][self.current_coordinate[0]] == "M": # 지뢰칸을 밟았는지 먼저 검사
+            bomb_img = pg.image.load("bomb.png")
+            bomb = pg.transform.scale(bomb_img, (cell_size, cell_size))
+            for x in range(cell_num):
+                for y in range(cell_num):
+                    if self.mine_coordinate[y][x] == "M":
+                        pg.draw.rect(self.screen, red, [margin + cell_size*x, margin + cell_size*y, cell_size, cell_size])
+                        self.screen.blit(bomb, (margin + cell_size*x, margin + cell_size*y))
+            self.draw_board()
+            return True
 
-def secure_cell(font, screen):
-    if mine_coordinate[current_coordinate[1]][current_coordinate[0]] == "M": # 지뢰칸을 밟았는지 먼저 검사
-        bomb_img = pg.image.load("bomb.png")
-        bomb = pg.transform.scale(bomb_img, (cell_size, cell_size))
-        for x in range(cell_num):
-            for y in range(cell_num):
-                if mine_coordinate[y][x] == "M":
-                    pg.draw.rect(screen, red, [margin + cell_size*x, margin + cell_size*y, cell_size, cell_size])
-                    screen.blit(bomb, (margin + cell_size*x, margin + cell_size*y))
-        draw_board(screen)
-        return True
-
-    if (current_coordinate[0], current_coordinate[1]) not in secured_coordinate:
-        secured_coordinate.append((current_coordinate[0], current_coordinate[1]))
-
-    draw_secured_cells(screen)
-    draw_board(screen)
-    show_numbers(font, screen)
-    draw_me(screen)
-    return False
+        if (self.current_coordinate[0], self.current_coordinate[1]) not in self.secured_coordinate:
+            self.secured_coordinate.append((self.current_coordinate[0], self.current_coordinate[1]))
         
-def exit():
-    pg.quit()
-    sys.exit()
+        if (self.current_coordinate[0], self.current_coordinate[1]) in self.showed_list:
+            self.showed_list.remove((self.current_coordinate[0], self.current_coordinate[1]))
+
+        self.draw_secured_cells()
+        self.draw_board()
+        self.show_numbers()
+        self.draw_me()
+        return False
+
+    def set_path(self):
+        x, y = 0, 0
+        while x+y < (cell_num-1)*2:
+            if random.random() > 0.5 and x < cell_num-1:
+                x += 1
+                self.mine_coordinate[y][x] = "P"
+            elif y < cell_num-1:
+                y += 1
+                self.mine_coordinate[y][x] = "P"
+
+    def set_mines(self, level):
+        if level == "easy":
+            mine_num = (cell_num**2)//2
+
+        minefield = list(range(1, cell_num**2))
+        for mine in range(1, cell_num**2):
+            if self.mine_coordinate[mine//cell_num][mine%cell_num] == "P":
+                minefield.remove(mine)
+        mine_list = random.sample(minefield, mine_num)
+        for mine in mine_list:
+            mine_X = mine%cell_num
+            mine_Y = mine//cell_num
+            self.mine_coordinate[mine_Y][mine_X] = "M"
+
+            self.coordinate[mine_Y][mine_X] += 1 # 지뢰가 있는 칸
+            if mine_Y > 0: # 상
+                self.coordinate[mine_Y - 1][mine_X] += 1
+            if mine_Y < cell_num-1: # 하
+                self.coordinate[mine_Y + 1][mine_X] += 1
+            if mine_X > 0: # 좌
+                self.coordinate[mine_Y][mine_X - 1] += 1
+            if mine_X < cell_num-1: # 우
+                self.coordinate[mine_Y][mine_X + 1] += 1
+            if mine_Y > 0 and mine_X > 0: # 좌상
+                self.coordinate[mine_Y - 1][mine_X - 1] += 1
+            if mine_Y < cell_num-1 and mine_X > 0: # 좌하
+                self.coordinate[mine_Y + 1][mine_X - 1] += 1
+            if mine_Y > 0 and mine_X < cell_num-1: # 우상
+                self.coordinate[mine_Y - 1][mine_X + 1] += 1
+            if mine_Y < cell_num-1 and mine_X < cell_num-1: # 우하
+                self.coordinate[mine_Y + 1][mine_X + 1] += 1
+
+        for i in self.mine_coordinate: print(i)
+
+    def exit(self):
+        pg.quit()
+        sys.exit()
+
+class Button(object):
+    def __init__(self, screen, game):
+        self.screen = screen
+        self.font = pg.font.SysFont("Times New Roman", margin*3//10)
+        self.game = game
+        self.draw_buttons()
+
+    def draw_buttons(self):
+        easy_rect = print_text(self.font, self.screen, "Easy", green, (margin + cell_size , (margin*3)//2 + cell_size*cell_num))
+        self.rects = [easy_rect]
+        return self.rects
+
+    def check_click(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.MOUSEBUTTONDOWN:
+                    if self.check_button(event.pos):
+                        level = self.check_button(event.pos)
+                        play_game(self.screen, self.game, level)
+                elif event.type == pg.QUIT:
+                    self.game.exit()
+        
+            pg.display.flip()
+            clock.tick(fps)
+
+    def check_button(self, pos):
+        if self.rects[0].collidepoint(pos):
+            level = "easy"
+            return level
+        return False
 
 main()
